4e55c972997d576886acf7fbe8e46d8a747a949d
DHWANIL018 <pradhwanil@gmail.com>
2025-03-25 12:03:31 +0530

    First commit

---
+++ firstprg.c
@@ -0,0 +1,30 @@
+// Shree Ganeshay namah 
+
+#include<stdio.h>
+
+int main(){
+    /// Integer 
+    int value1 = 33;
+    int value2 = 44;
+
+    printf("%d",value1);
+    printf("\n%d",value2);
+
+    // float 
+
+    float f1 = 33.33;
+    printf("\n%f",f1);
+
+    // chatacter 
+    char Char = 'a';
+    printf("\n%c",Char);
+
+    // string
+
+    char name[] = "Dhwanil";
+    printf("\n%s",name);
+    
+
+    
+    
+}
---
+++ intro.c
@@ -0,0 +1,15 @@
+// Shree Ganeshay namah 
+
+#include<stdio.h>
+    int main(){
+
+        printf("\nJay Shree ram");
+
+        printf("\t c Language \n");
+        printf("Second Line");
+
+        // /t for tab Space and /n For Line
+
+        printf("\nThird Line");
+        return 0;
+    }
